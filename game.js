257fa421f0ab0c0820910d883cd1702031589539
snoozie <120746041+snoozie-v@users.noreply.github.com>
2023-10-18 08:02:00 -0700

    change the logic to fail the game
    previously the user had to input a sequence as long as the game pattern
    
    this change makes it so the wrong sound plays as soon as the user inputs the wrong color

--- game.js
+++ game.js
@@ -4,7 +4,6 @@ var userPattern = [];
 var level = 0;
 var playingPattern = false;
 
-// Define audio elements
 var greenSound = new Audio('./sounds/green.mp3');
 var redSound = new Audio('./sounds/red.mp3');
 var yellowSound = new Audio('./sounds/yellow.mp3');
@@ -33,8 +32,7 @@ function showPattern() {
     userPattern = [];
     level++;
     $("#level-title").text("Level " + level);
-
-    // Generate and show the next step in the pattern
+    
     var randomColor = buttonColors[Math.floor(Math.random() * 4)];
     gamePattern.push(randomColor);
 
@@ -47,40 +45,47 @@ function showPattern() {
             }, 100);
         }, 500 * index);
     });
-
-    // Allow user interaction after showing the pattern
     setTimeout(function () {
         playingPattern = false;
     }, 500 * gamePattern.length);
 }
 
 $(".btn").click(function () {
-    if (playingPattern) return; // Prevent user interaction during pattern playback
+    if (playingPattern) return;
 
     var userColor = $(this).attr("id");
+    console.log(userColor)
     userPattern.push(userColor);
     playSound(userColor);
 
-    // Check if the user's input matches the pattern
     if (userPattern.length === gamePattern.length) {
         if (JSON.stringify(userPattern) === JSON.stringify(gamePattern)) {
-            // User successfully repeated the pattern
             setTimeout(function () {
                 showPattern();
             }, 1000);
         } else {
             wrongSound.play()
             $("#level-title").text("Game Over, Click to Restart.");
-            // Reset gamePattern and userPattern if needed
+
+            gamePattern = [];
+            userPattern = [];
+            level = 0;
+            playingPattern = false;
+        }
+    } else {
+        if (userPattern[userPattern.length - 1] !== gamePattern[userPattern.length - 1]) {
+            wrongSound.play()
+            $("#level-title").text("Game Over, Click to Restart.");
+ 
             gamePattern = [];
             userPattern = [];
             level = 0;
             playingPattern = false;
         }
     }
-});
+    }
+);
 
-// Start the game by showing the initial pattern
 $("#start-button").click(function () {
     if (!playingPattern && level === 0) {
         showPattern();
